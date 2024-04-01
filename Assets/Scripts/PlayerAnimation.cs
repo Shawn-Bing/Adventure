604fb9db49326a36a02a2507ad583f2adbc322a8
Shawn-Bing <icyoung2020@163.com>
2024-04-01 21:02:58 +0800

    Finish Jump&Landing state machine

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -7,20 +7,20 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator ani;
+    private PhysicsDetection phsicDtc;
 
     private void SetAnimation()
     {
-        //将人物X轴移动速度赋值给我们创建的名为velocityX的浮点数
-
-        //由于Velocity值是通过方向（带正负值）和速度乘积获得的，因此当向左时为负
-        //我们设置的动画切换条件也只有大于某数值时切换，为了减少判断次数，这里可以直接将最后的vX值取绝对值
         ani.SetFloat("velocityX",Math.Abs(rb.velocity.x));
+        ani.SetFloat("velocityY",rb.velocity.y);//不用去掉负值
+        ani.SetBool("isGround",phsicDtc.isGround);//获取物理检测中地面监测
     }
 
     //获取对应组件
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
+        phsicDtc = GetComponent<PhysicsDetection>();
     }
     //每帧检测调用，切换动画
     private void Update() {
