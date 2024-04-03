d598cc95d50dbf6cac9bc2df25b4572c3c4beb54
Shawn-Bing <icyoung2020@163.com>
2024-04-03 20:08:39 +0800

    Codes improved,Add Notes

--- Assets/Scripts/General/Properties.cs
+++ Assets/Scripts/General/Properties.cs
@@ -12,30 +12,39 @@ public class Properties : MonoBehaviour
 
     [Header("无敌时间")]
     public float invulnerableDuration;//无敌时间，这个值在引擎中设置
-    public float invulnerableCounter;//计时器，将Duration量化为引擎时间
-    public bool invulnerable;//检测是否无敌
+    private float invulnerableCounter;//计时器，将Duration量化为引擎时间
+    public bool isInvulnerable;//检测是否无敌
     
-    //触发无敌
-    private void TriggerInvulnerable()
+    //开启无敌
+    private void Invulnerable()
     {
-        //（保证受击后不会重复进入无敌再退出）
-        if(!invulnerable){
-            invulnerable = true;
-            invulnerableCounter = invulnerableDuration;
+        isInvulnerable = true;
+        invulnerableCounter = invulnerableDuration;
+    }
+    //当触发了无敌时间时,在Update中使用这个计时器
+    private void InvulnerableTimer()
+    {
+        if(isInvulnerable)
+        {
+            invulnerableCounter -= Time.deltaTime;//让机器读秒运行
+            
+            //当计时器归零，退出无敌状态
+            //为什么不是==0？因为以帧为单位递减很快就会减少到小于0
+            if(invulnerableCounter <= 0)
+            isInvulnerable = false;
         }
     }
-
 
     //受击者接收来自攻击者的伤害
     public void TakeDamage(Attack attacker)
     {
-        if(invulnerable)return;//若无敌，提前结束伤害计算
+        if(isInvulnerable)return;//若无敌，提前结束伤害计算
         
         //若此次攻击扣血后生命值为正
         if(currentHealth - attacker.attackDamage > 0)
         {
-            currentHealth -= attacker.attackDamage;//若收到伤害，开启短暂无敌
-            TriggerInvulnerable();
+            currentHealth -= attacker.attackDamage;//若收到伤害，开启无敌
+            Invulnerable();
         }else{
             //血量小于攻击伤害值，使其为0
             currentHealth = 0;
@@ -46,15 +55,7 @@ public class Properties : MonoBehaviour
         currentHealth = maxHealth;//初始化生命值
     }
     private void Update() {
-        //当触发了无敌时间时
-        if(invulnerable)
-        {
-            //在Update中使用计时器
-            invulnerableCounter -= Time.deltaTime;
-            //当计时器归零，退出无敌状态
-            if(invulnerableCounter <= 0)
-            invulnerable = false;
-        }
+        InvulnerableTimer();
     }
 
     //测试碰撞
