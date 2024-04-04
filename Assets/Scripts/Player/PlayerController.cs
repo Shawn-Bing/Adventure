8fab1da25eae5cfe8f9d1d259660e6496aaea9c1
Shawn-Bing <icyoung2020@163.com>
2024-04-04 20:07:40 +0800

    Fixed the Collider Size&Offset which won't change when player Crouch

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,12 +8,12 @@ public class PlayerController : MonoBehaviour
 {
 
     #region 创建类实例
-
     public PlayerInputController inputControl;
     public Vector2 inputDirection;
 
     private Rigidbody2D rb;
     private PhysicsDetection physicsDetection;
+    private CapsuleCollider2D cpsCld2D;
 
     #endregion
 
@@ -22,6 +22,10 @@ public class PlayerController : MonoBehaviour
     public float JumpForce;
     private float currentSpeed;
     public bool isCrouch;//存放下蹲状态
+
+    //保存原始Size&Offset的变量
+    private Vector2 originCapsuleCollider2DSize;
+    private Vector2 originCapsuleCollider2DOffset;
 
     public void Movement()
     {
@@ -34,12 +38,24 @@ public class PlayerController : MonoBehaviour
         transform.localScale = new Vector3(playerDir, 1, 1);
         //Crouch
         isCrouch = physicsDetection.isGround && inputDirection.y < -0.1f;//在地面，且垂直速度为负（按了下方向键）
-        
+        if (isCrouch)
+        {
+            //修改碰撞体Size&Offset
+                //此数据是自己尝试来的
+            cpsCld2D.offset = new Vector2(originCapsuleCollider2DOffset.x, 0.7f);//Michele：0.85
+            cpsCld2D.size = new Vector2(originCapsuleCollider2DSize.x, 1.4f);//Michele：1.7
+        }
+        else
+        {
+            //还原Size&Offset
+            cpsCld2D.offset = originCapsuleCollider2DOffset;
+            cpsCld2D.size = originCapsuleCollider2DSize;
+        }
     }
     private void Jump(InputAction.CallbackContext context)
     {
         //检测是否在地面，若是，允许跳跃
-        if(physicsDetection.isGround)
+        if (physicsDetection.isGround)
         {
             rb.AddForce(transform.up * JumpForce, ForceMode2D.Impulse);
         }
@@ -47,10 +63,14 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        #region 获取组件
         rb = GetComponent<Rigidbody2D>();
-        //获取物理检测
         physicsDetection = GetComponent<PhysicsDetection>();
+        cpsCld2D = GetComponent<CapsuleCollider2D>();
+
         inputControl = new PlayerInputController();
+        #endregion
+
         //绑定Jump函数到Jump按键按下
         inputControl.GamePlay.Jump.started += Jump;
 
@@ -61,17 +81,20 @@ public class PlayerController : MonoBehaviour
         //修改速度并绑定到潜行键
         inputControl.GamePlay.Sneak.performed += ctx =>
         {
-            if(physicsDetection.isGround)
-            speedScale = currentSpeed/2.5f;
+            if (physicsDetection.isGround)
+                speedScale = currentSpeed / 2.5f;
         };
         //松开键位后速度回正
         inputControl.GamePlay.Sneak.canceled += ctx =>
         {
-            if(physicsDetection.isGround)
-            speedScale = currentSpeed;
+            if (physicsDetection.isGround)
+                speedScale = currentSpeed;
         };
         #endregion
-        
+
+        //保存原始碰撞体数据，要在一开始就获取
+        originCapsuleCollider2DSize = cpsCld2D.size;
+        originCapsuleCollider2DOffset = cpsCld2D.offset;
     }
     private void OnEnable()
     {
